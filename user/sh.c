b3c0fcb14bb3aa9a482fe808a4dcefbb812725fb
Job Noorman <job@noorman.info>
2020-11-23 12:04:59 +0100

    Make sh execute the .shinit script on startup when it exists

--- user/sh.c
+++ user/sh.c
@@ -141,6 +141,45 @@ getcmd(char *buf, int nbuf)
   return 0;
 }
 
+static void
+processline(char* buf)
+{
+  if(buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' '){
+    // Chdir must be called by the parent, not the child.
+    buf[strlen(buf)-1] = 0;  // chop \n
+    if(chdir(buf+3) < 0)
+      fprintf(2, "cannot cd %s\n", buf+3);
+    return;
+  }
+  if(fork1() == 0)
+    runcmd(parsecmd(buf));
+  wait(0);
+}
+
+static int
+execute_script(const char* path)
+{
+  int fd = open(path, O_RDONLY);
+
+  if (fd == -1) {
+    return -1;
+  }
+
+  char buf[100];
+
+  while (1) {
+    fgets(buf, sizeof(buf), fd);
+
+    if (buf[0] == '\0') {
+      break;
+    }
+
+    processline(buf);
+  }
+
+  return 0;
+}
+
 int
 main(void)
 {
@@ -155,18 +194,12 @@ main(void)
     }
   }
 
+  // Read and process .shinit if it exists.
+  execute_script(".shinit");
+
   // Read and run input commands.
   while(getcmd(buf, sizeof(buf)) >= 0){
-    if(buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' '){
-      // Chdir must be called by the parent, not the child.
-      buf[strlen(buf)-1] = 0;  // chop \n
-      if(chdir(buf+3) < 0)
-        fprintf(2, "cannot cd %s\n", buf+3);
-      continue;
-    }
-    if(fork1() == 0)
-      runcmd(parsecmd(buf));
-    wait(0);
+    processline(buf);
   }
   exit(0);
 }
